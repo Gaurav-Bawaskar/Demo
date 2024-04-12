76d2dc47f7fabeb3cc1cf0c937211aa9c15b7283
Gaurav <Gaurav@LAPTOP-1NHGUAPF>
2024-04-12 14:50:14 +0530

    commite class2

--- Demo_Git_Project/src/Demo_git/My_Git_Class.java
+++ Demo_Git_Project/src/Demo_git/My_Git_Class.java
@@ -1,7 +1,7 @@
 package Demo_git;
 
 public class My_Git_Class {
-public static void main(String[] args) {
-	System.out.println("Hello");
-}
+   public static void main(String[] args) {
+	  System.out.println("Hello");
+   }
 }
---
+++ Demo_Git_Project/src/Demo_git/My_Git_Class2.java
@@ -0,0 +1,7 @@
+package Demo_git;
+
+public class My_Git_Class2 {
+	public static void main(String[] args) {
+		System.out.println("Hlw Git");
+	}
+}
